9eb411e312d0a858c3899c3111b12c1ef8ba051e
Matheus Torrão <matheustorraox@gmail.com>
2025-10-10 19:08:57 -0300

    test: Modificando a função que retornar 200 na rota raiz

--- tests/app.test.js
+++ tests/app.test.js
@@ -15,6 +15,6 @@ describe("Testando servidor", () => {
   it("Deve responder na rota raiz / com status 200", async () => {
     const res = await request(server).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty("message", "Servidor Funcionando");
+    expect(res.body).toHaveProperty("message", "Servidor Funcionando!");
   });
 });
