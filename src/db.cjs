b559f5ee678d0594a3fe1150a847ee5b90a0a1b1
Matheus Torrão <matheustorraox@gmail.com>
2026-01-12 21:07:37 -0300

    feat(Modifcação): Arquivo foi modificado a forma de importar o PrismaClient e a instancia!

--- src/db.cjs
+++ src/db.cjs
@@ -1,5 +1,11 @@
-const { PrismaClient } = require('./generated/prisma');
-
-const prisma = new PrismaClient();
-
+const { PrismaClient } = require('@prisma/client');
+const dotenv = require('dotenv');
+const prisma = new PrismaClient({
+    datasourceURL: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
+console.log('Conexão com o banco de dados estabelecida');
 module.exports = { prisma };
