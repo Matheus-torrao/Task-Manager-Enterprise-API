c46dc8fecca77fdbcff2c150b9f959456a7a12d2
Matheus Torrão <matheustorraox@gmail.com>
2026-01-18 01:08:08 -0300

    ainda corrigindo o bug de importação do PrismaClient

--- src/db.cjs
+++ src/db.cjs
@@ -1,13 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
-const path = require('path');
+const { PrismaClient } = require('./generate/prisma');
 
-// Mantemos o que funcionou para carregar o .env
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
+const prisma = new PrismaClient();
+module.exports = prisma;
 console.log('--- Sistema de Banco de Dados ---');
 console.log('Conexão configurada:', process.env.DATABASE_URL ? 'OK' : 'FALHA');
-
-// Use o construtor VAZIO. O Prisma 7 lerá o DATABASE_URL automaticamente.
-const prisma = new PrismaClient();
-
-module.exports = { prisma };
