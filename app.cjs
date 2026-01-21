fb5f2410a66b9c518bc444180f0cd6aae5666e6f
Matheus Torrão <matheustorraox@gmail.com>
2026-01-20 22:43:40 -0300

    correção de instancia de módulos duplicados

--- app.cjs
+++ app.cjs
@@ -1,12 +1,10 @@
 
 const express = require('express');
 const cors = require ('cors');
-const bodyParser = require('body-parser');
 
 // Configuração de Middlewares
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 app.use(express.json());
 
 // Importando as rotas e aplicando um Roteador prefixo
@@ -20,7 +18,7 @@ app.get('/', (req, res) => {
 
 const PORT = 3000
 app.listen(PORT, ()=>{
-    console.log(`Servidor rodando em http://localhost${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 module.exports = app;
