a1c9e85c48f5566a50516ac933af45e4d38206e1
Matheus Torrão <matheustorraox@gmail.com>
2026-02-18 02:52:53 -0300

    Refactor: Refatorando arquivo docker-compose

--- src/db.cjs
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('./generate/prisma');
-
-const prisma = new PrismaClient();
-module.exports = prisma;
-console.log('--- Sistema de Banco de Dados ---');
-console.log('Conexão configurada:', process.env.DATABASE_URL ? 'OK' : 'FALHA');
